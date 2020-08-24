f55c98addd0388c38bb237a9b5b50f5d19199986
Danilo <danilo_augusto_95@hotmail.com>
2020-08-24 16:44:50 -0300

    Implementação de data migration com flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,3 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo, São Paulo, Brasil', 'Danilo', 'Male', 'Silva'),
+	(2, 'Lua Prata, Reinos do Leste, Azeroth', 'Sylvannas', 'Female', 'Correventos');
