5c593eab32c537ba37a8c9f29a288d04fdc812c2
Danilo <danilo_augusto_95@hotmail.com>
2020-09-10 16:17:33 -0300

    PersonVO, colocado personalização no JSON de retorno da api, invez de key, aparece id e ordem dos atributos

--- src/main/java/com/porik/udemyspring/data/vo/v1/PersonVO.java
+++ src/main/java/com/porik/udemyspring/data/vo/v1/PersonVO.java
@@ -10,23 +10,24 @@ import org.springframework.hateoas.RepresentationModel;
 import java.io.Serializable;
 import java.util.Objects;
 
-//@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"}) //Muda a ordem como os atributos aparece na resposta da API
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"}) //Muda a ordem como os atributos aparece na resposta da API
 public class PersonVO extends RepresentationModel implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Mapping("id")
+    @JsonProperty("id")
     private Long key;
 
-//    @JsonProperty("last_name") //Muda o nome como o atributo aparece na resposta da API
+    @JsonProperty("first_name") //Muda o nome como o atributo aparece na resposta da API
     private String firstName;
 
-//    @JsonProperty("last_name") //Muda o nome como o atributo aparece na resposta da API
+    @JsonProperty("last_name") //Muda o nome como o atributo aparece na resposta da API
     private String lastName;
 
     private String address;
 
-//    @JsonIgnore //Ignora o atributo, ele não aparecerá na resposta da API
+    @JsonIgnore //Ignora o atributo, ele não aparecerá na resposta da API
     private String gender;
 
     public PersonVO() {
